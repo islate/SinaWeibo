db88af85c476afecdb33c32a5132e7166fd6b3ba
lyz <linyize@gmail.com>
2017-08-15 21:27:27 +0800

    0.1.0

---
+++ SinaWeibo/Global.h
@@ -0,0 +1,14 @@
+//
+//  Global.h
+//  weiboSDK
+//
+//  Created by yizelin on 13-3-22.
+//  Copyright (c) 2013年 yizelin. All rights reserved.
+//
+
+#ifndef weiboSDK_Global_h
+#define weiboSDK_Global_h
+
+#import "SinaWeibo.h"
+
+#endif
